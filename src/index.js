82bf857afa1e1b204371e0d7af3bd0318fc89851
Pedro Augusto <79765850+PedroASB@users.noreply.github.com>
2025-09-25 16:36:29 -0300

    feat: create the homepage for the restaurant
    - HTML hard-coded version of the homepage implemented
    - CSS styles added
    - Custom fonts and homepage images added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
+import "./reset.css";
 import "./style.css";
+
+import image1 from "./imgs/hamburger-1.jpg";
+import image2 from "./imgs/hamburger-2.jpg";
+import image3 from "./imgs/sandwich.jpg";
+import image4 from "./imgs/dessert.jpg";
+import cdIcon from "./imgs/cd-icon.png";
+
+// Temporary
+const imageDiv1 = document.querySelector("#image-1");
+const imageDiv2 = document.querySelector("#image-2");
+const imageDiv3 = document.querySelector("#image-3");
+const imageDiv4 = document.querySelector("#image-4");
+
+const imageDivs = [imageDiv1, imageDiv2, imageDiv3, imageDiv4];
+const imagesPaths = [
+    image1,
+    image2,
+    image3,
+    image4,
+];
+
+for (let i = 0; i < 4; i++) {
+    const src = imagesPaths[i];
+    const img = document.createElement("img");
+    img.setAttribute("src", src);
+    imageDivs[i].appendChild(img);
+}
+
+const menuButtons = Array.from(document.querySelectorAll("nav button"));
+console.log(menuButtons);
+
+menuButtons.forEach((button) => {
+    const cdIconImg = document.createElement("img");
+    cdIconImg.setAttribute("src", cdIcon);
+    button.insertBefore(cdIconImg, button.firstChild);
+});
